853c4cf7ccabd5b68b5200a411bef44c70ec8083
Dagur Leo <dagurl14@ru.is>
2015-10-29 13:25:22 +0000

    Added testideas - non implemented

--- src/testTTT/TestTTT.java
+++ src/testTTT/TestTTT.java
@@ -1,1 +1,58 @@
-Test implement
+package testTTT;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTTT {
+	public static void main(String arg[])
+	{
+		org.junit.runner.JUnitCore.main("testTTT.TestTTT");
+	}
+	
+	@Test
+	public void checkXIsWinnerFirstRow()
+	{
+		
+	}
+	
+	@Test
+	public void checkOIsWinnerSecondRow()
+	{
+		
+	}
+	
+	@Test
+	public void CheckLeftLeaningX()
+	{
+
+	}
+	
+	@Test
+	public void CheckRightLeaningO()
+	{
+		
+	}
+
+	@Test
+	public void CheckMoveOverwrite()
+	{
+		
+	}
+
+	@Test
+	public void CheckIfTie()
+	{
+		
+	}
+	@Test
+	public void CheckIfPlayAgain()
+	{
+		
+
+	}
+	@Test
+	public void CheckIfMoveNotIn3x3()
+	{
+		
+	} 
+}
