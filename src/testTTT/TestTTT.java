f27f04f2dc4d61d2497b0b1ac258a5c5dc1b4de5
Daniel <danniing@gmail.com>
2015-10-29 15:40:18 +0000

    Added TTT-tests

--- src/testTTT/TestTTT.java
+++ src/testTTT/TestTTT.java
@@ -12,47 +12,56 @@ public class TestTTT {
 	@Test
 	public void checkXIsWinnerFirstRow()
 	{
-		
+		TicTacToe TTTGame = new TicTacToe();
+		TTTGame.makeMove('X', 0, 0);
+		Assert.assertEquals(false, TTTGame.checkForWinner('X'));
+		TTTGame.makeMove('O', 1, 0);
+		Assert.assertEquals(false, TTTGame.checkForWinner('O'));
+		TTTGame.makeMove('X', 0, 1);
+		Assert.assertEquals(false, TTTGame.checkForWinner('X'));
+		TTTGame.makeMove('O', 1, 1);
+		Assert.assertEquals(false, TTTGame.checkForWinner('O'));
+		TTTGame.makeMove('X', 0, 2);
+		Assert.assertEquals(true,TTTGame.checkForWinner('X'));
+		//TTTGame.print();
 	}
 	
 	@Test
 	public void checkOIsWinnerSecondRow()
 	{
-		
+		TicTacToe TTTGame = new TicTacToe();
+		TTTGame.makeMove('X', 0, 0);
+		Assert.assertEquals(false, TTTGame.checkForWinner('X'));
+		TTTGame.makeMove('O', 1, 0);
+		Assert.assertEquals(false, TTTGame.checkForWinner('O'));
+		TTTGame.makeMove('X', 2, 2);
+		Assert.assertEquals(false, TTTGame.checkForWinner('X'));
+		TTTGame.makeMove('O', 1, 1);
+		Assert.assertEquals(false, TTTGame.checkForWinner('O'));
+		TTTGame.makeMove('X', 0, 2);
+		Assert.assertEquals(false, TTTGame.checkForWinner('X'));
+		TTTGame.makeMove('O', 1, 2);
+		Assert.assertEquals(true,TTTGame.checkForWinner('O'));
+		TTTGame.print();
 	}
 	
 	@Test
 	public void CheckLeftLeaningX()
 	{
-
+		TicTacToe TTTGame = new TicTacToe();
+		TTTGame.makeMove('X', 0, 0);
+		Assert.assertEquals(false, TTTGame.checkForWinner('X'));
+		TTTGame.makeMove('O', 1, 0);
+		Assert.assertEquals(false, TTTGame.checkForWinner('O'));
+		TTTGame.makeMove('X', 1, 1);
+		Assert.assertEquals(false, TTTGame.checkForWinner('X'));
+		TTTGame.makeMove('O', 1, 2);
+		Assert.assertEquals(false, TTTGame.checkForWinner('O'));
+		TTTGame.makeMove('X', 2, 2);
+		Assert.assertEquals(true, TTTGame.checkForWinner('X'));
+		TTTGame.print();
 	}
 	
 	@Test
 	public void CheckRightLeaningO()
-	{
-		
-	}
-
-	@Test
-	public void CheckMoveOverwrite()
-	{
-		
-	}
-
-	@Test
-	public void CheckIfTie()
-	{
-		
-	}
-	@Test
-	public void CheckIfPlayAgain()
-	{
-		
-
-	}
-	@Test
-	public void CheckIfMoveNotIn3x3()
-	{
-		
-	} 
 }
