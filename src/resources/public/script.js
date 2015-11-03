cd146a065d358bda4784c332d762d1165c3871e5
Hlynur Hansen <hlynur14@i-7f3b5dd9.csd07cloud.internal>
2015-11-03 00:57:02 +0000

    Spark integrated into solution

--- src/resources/public/script.js
+++ src/resources/public/script.js
@@ -1,20 +1,25 @@
-var p1, p2;
+var p1, p2, p; 
+var thereIsAWinner = false;
 function nameFunction() {
     p1 = document.getElementById("p1").value;
     p2 = document.getElementById("p2").value;
+    if (p1 == "" || p2 == "")
+    {
+	return;
+    }
     var names = {
-        "name1": p1,
-        "name2": p2
+        "name1":p1,
+        "name2":p2
     }
     var json = JSON.stringify(names);
     $.ajax({
-        url:"/newGame",
-        type: "post",
-        datatype: "JSON",
+        url:'/newGame',
+        type: 'post',
+        datatype: 'json',
         data: json,
         contentType: 'application/json; charset=utf-8',
         success: function() {
-            alert("smegma");
+            //alert(json);
         }
 
     });
@@ -29,31 +34,47 @@ function nameFunction() {
 }
 
 function newGame () {
-    var name1 = $("#p1").val();
-    var name2 = $("#p2").val();
-	var names = {
-	    "name1": name1,
-	    "name2": name2
-	}
-		
-    /*var json = JSON.stringify(names);
-		$.ajax({  
-            type: post,
-            url: 'TTTWeb/newGame',
-            datatype: "json",
-	        data: json,
-            success: function(response) {
-			   
-			   
-			 }
-            });*/
-    $(".popUp").hide();
-    $("#names").text(name1 + " vs  " + name2);	
+   var pop;
+   pop = document.getElementsByClassName("popUp");
+   pop[0].style.visibility="visible";
+   document.getElementById("1").innerHTML = "";
+   document.getElementById("2").innerHTML = "";
+   document.getElementById("3").innerHTML = "";
+   document.getElementById("4").innerHTML = "";
+   document.getElementById("5").innerHTML = "";
+   document.getElementById("6").innerHTML = "";
+   document.getElementById("7").innerHTML = "";
+   document.getElementById("8").innerHTML = "";
+   document.getElementById("9").innerHTML = "";
+   thereIsAWinner = false;
 };
 
 function pressTile(clicked_id) {
     var tile = document.getElementById(clicked_id);
-    if(tile.innerHTML == "")
+    var number = clicked_id;
+    var stuff = {
+	"id":number
+    }
+    var json = JSON.stringify(stuff);
+    var makeMove;
+    var IsBoardFull;
+    var Winner;
+    var name; 
+    $.ajax({
+        url:'/move',
+        type: 'post',
+        datatype: 'json',
+        data: json,
+        contentType: 'application/json; charset=utf-8',
+	async: false,
+        success: function(data) {
+	    makeMove = JSON.parse(data).makeMove;
+	    IsBoardFull = JSON.parse(data).IsBoardFull;
+	    Winner = JSON.parse(data).Winner;
+	    name = JSON.parse(data).Name;
+        }
+    });
+    if(tile.innerHTML == "" && makeMove == true && thereIsAWinner == false)
     {
         tile.innerHTML = p;
         if(p == "X")
@@ -65,10 +86,29 @@ function pressTile(clicked_id) {
             p = "X";
         }
     }
+    if (Winner && thereIsAWinner == false) { thereIsAWinner = true; alert("We have a winner: " + name)}
+    if (IsBoardFull) { thereIsAWinner = true; alert("TIE!!!!!")}
 };
 
 function quitGame()
 {
+   p1 = "";
+   p2 = "";
+   var pop;
+   pop = document.getElementsByClassName("popUp");
+   pop[0].style.visibility="visible";
+   document.getElementById("p1").value = "";
+   document.getElementById("p2").value = "";
+   document.getElementById("1").innerHTML = "";
+   document.getElementById("2").innerHTML = "";
+   document.getElementById("3").innerHTML = "";
+   document.getElementById("4").innerHTML = "";
+   document.getElementById("5").innerHTML = "";
+   document.getElementById("6").innerHTML = "";
+   document.getElementById("7").innerHTML = "";
+   document.getElementById("8").innerHTML = "";
+   document.getElementById("9").innerHTML = "";
+   thereIsAWinner = false;
 
 }
 
